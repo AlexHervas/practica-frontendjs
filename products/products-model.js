ca31df749f3daa052aef3b429ed818c2e0e8460f
Alejandro Hervás <alesepto15@gmail.com>
2024-11-29 22:59:22 +0100

    api connected using fetch on products-model.js, try catch to throw an error if the connection fails.

---
+++ products/products-model.js
@@ -0,0 +1,15 @@
+export async function getProducts() {
+    try {
+      const response = await fetch("http://localhost:8000/api/products");
+      const products = await response.json();
+      
+      if (!response.ok) {
+        throw new Error("Recurso no existente");
+        }
+  
+      return products;
+  
+    } catch (error) {
+      throw new Error(error.message)
+    }
+}
