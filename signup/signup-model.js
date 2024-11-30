4de09316f902e1ff21388cd0662554055e87ed50
Alejandro Hervás <alesepto15@gmail.com>
2024-11-30 19:03:16 +0100

    Added signup functionality throw sparrest api. Made utils folder with constant values.

---
+++ signup/signup-model.js
@@ -0,0 +1,21 @@
+export async function createUser(email, password){
+  try {
+      const response = await fetch("http://localhost:8000/auth/register", {
+        method: "POST",
+        body: JSON.stringify({
+          username: email,
+          password
+        }),
+        headers: {
+          "Content-type": "application/json"  
+        }
+      });
+    
+      if (!response.ok) {
+        throw new Error("Error creando usuario")
+      }
+    
+    } catch (error) {
+        throw new Error(error.message)
+    }
+  }
